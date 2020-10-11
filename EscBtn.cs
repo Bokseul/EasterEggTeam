da8355be707226cc8edced5614d58246b2455d75
Bokseul <63533668+Bokseul@users.noreply.github.com>
2020-10-12 00:29:25 +0900

    네트워크

---
+++ EscBtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscBtn : MonoBehaviourPunCallbacks
+{
+    public Button ExitBtn;
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RunningNetwork.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+public class RunningNetwork : MonoBehaviourPun
+{
+    public GameObject floor;
+    private GameObject Char;
+    private int num;
+
+    public Transform[] spawnPoints = new Transform[4];
+        
+    void Awake()
+    {
+        Vector3 spawn;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            spawn = spawnPoints[Random.Range(0, 4)].position;
+            //spawn = new Vector3(floor.transform.position.x, floor.transform.position.y + 10, floor.transform.position.z);
+        }
+        else
+        {
+            spawn = spawnPoints[Random.Range(0, 4)].position;
+            //num += 10;
+            //spawn = new Vector3(floor.transform.position.x + num, floor.transform.position.y + 10, floor.transform.position.z);
+        }
+        Char = PhotonNetwork.Instantiate("Player", spawn, Quaternion.identity);
+        // Char = Instantiate(Char, spawn, Quaternion.identity);
+       Camera.main.GetComponent<CharacterCamera>().SetTarget(Char);
+    }
+
+        
+}
